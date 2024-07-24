d9d1f7d917adab1b3550d3fae72493ffd84ca310
Zaahiid <zahidrsheikh01@gmail.com>
2024-07-25 00:26:43 +0530

    Installed the Shadcn 🎨

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="bg-red-200">Hello World</h1>
+      <Button>Test button</Button>
     </>
   );
 }
